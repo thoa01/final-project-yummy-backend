25d59e7f16a111ed7c74a35d27737f922ee65e6c
Thoa <thoathoa0110@gmail.com>
2024-03-03 00:29:04 +0700

    feature/disable-jwt-guard

--- src/auth/jwt-auth.guard.ts
+++ src/auth/jwt-auth.guard.ts
@@ -1,11 +1,24 @@
 import { ExecutionContext, Injectable, UnauthorizedException } from '@nestjs/common'
+import { Reflector } from '@nestjs/core'
 import { AuthGuard } from '@nestjs/passport'
+import { IS_PUBLIC_KEY } from 'src/decorator/customize'
 
 @Injectable()
 export class JwtAuthGuard extends AuthGuard('jwt') {
+  constructor(private reflector: Reflector) {
+    //Reflector lấy metadata ra
+    super()
+  }
+
   canActivate(context: ExecutionContext) {
-    // Add your custom authentication logic here
-    // for example, call super.logIn(request) to establish a session.
+    //ns cho nestjs khi truyền thêm thông tin IS_PUBLIC_KEY thì làm gì
+    const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
+      context.getHandler(),
+      context.getClass()
+    ]) //lấy metadata gửi kèm vs request này //truyền key(IS_PUBLIC_KEY) để lấy value ra //nếu JwtAuthGuard return true thì qua được JwtAuthGuard
+    if (isPublic) {
+      return true
+    }
     return super.canActivate(context)
   }
 
