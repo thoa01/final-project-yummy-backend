af707cfb5322b4796e80f7357065ceec33e66616
Thoa <thoathoa0110@gmail.com>
2024-03-04 23:23:35 +0700

    feature/transform-response

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { NestFactory, Reflector } from '@nestjs/core'
 import cookieParser from 'cookie-parser'
 import { AppModule } from './app.module'
 import { JwtAuthGuard } from './auth/jwt-auth.guard'
+import { TransformInterceptor } from './core/transform.interceptor'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
@@ -11,6 +12,7 @@ async function bootstrap() {
   const reflector = app.get(Reflector)
   app.useGlobalGuards(new JwtAuthGuard(reflector)) //dùng JwtAuthGuard global, bảo vệ tất cả route // ko truyền lên jwt thì kh thể truy cập
   app.useGlobalPipes(new ValidationPipe()) //config to data validation in dto
+  app.useGlobalInterceptors(new TransformInterceptor()) //config interceptor
   app.use(cookieParser()) //config cookies (lấy coookie từ fe hoặc set cookie cho fe)
   app.enableCors({ origin: '*', methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', preflightContinue: false }) //config cors
   await app.listen(configService.get('PORT')) //how to use .env in main (special)
